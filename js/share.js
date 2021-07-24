1d65c20ffb1699f4a53577171d088fd552a5c04c
zakigaebal <zakigaebal@gmail.com>
2021-07-24 18:58:10 +0900

    wqdqwdq

--- js/share.js
+++ js/share.js
@@ -3,7 +3,7 @@ const url = 'https://www.namoham.com/pigtest';
 function setShare(){
   var resultImg = document.querySelector('#resultImg');
   var resultAlt = resultImg.firstElementChild.alt;
-  const shareTitle = '십이간지 연애유형 결과';
+  const shareTitle = '당신의 결과는???';
   const shareDes = infoList[resultAlt].name;
   const shareImage = url + 'img/image-' + resultAlt + '.png';
   const shareURL = url + 'page/result-' + resultAlt + '.html';
